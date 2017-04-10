7783dc5cf9ae8e35a0b7149150f016b450120856
sibtain10 <sibtain.abbas10@gmail.com>
2017-04-10 15:13:11 +0500

    serverless without jwt

--- index.py
+++ index.py
@@ -1,20 +1,21 @@
 from __future__ import print_function
 
 import json
-import jwt
+# import jwt
 
-JWT_EXP_DELTA = 365
-JWT_SECRET = 'BlaBla'
-JWT_ALGO = 'HS256'
+# JWT_EXP_DELTA = 365
+# JWT_SECRET = 'BlaBla'
+# JWT_ALGO = 'HS256'
 
 def lambda_handler(event, context):
-    payload = {
-        'username': 'sib',
-        'exp': datetime.utcnow() + timedelta(days=JWT_EXP_DELTA)
-    }
+	return "Hello from Lambda!"
+    # payload = {
+    #     'username': 'sib',
+    #     'exp': datetime.utcnow() + timedelta(days=JWT_EXP_DELTA)
+    # }
     
-    #Encode the payload 
-    encoded_token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGO)
-    if encoded_token:
-        response_body = {'token': encoded_token}
-        return response_body
+    # #Encode the payload 
+    # encoded_token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGO)
+    # if encoded_token:
+    #     response_body = {'token': encoded_token}
+    #     return response_body
