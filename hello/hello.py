1f1f5cc527b27c1bd87a72c6c87d5407e1693211
sibtain10 <sibtain.abbas10@gmail.com>
2017-04-14 12:07:54 +0500

    installing pyJWT

--- hello/hello.py
+++ hello/hello.py
@@ -3,5 +3,5 @@ from index import my_func
 
 def lambda_handler(event, context):
 	print("Hello start")
-	my_func()
+	print(my_func())
 	print("Hello end")
--- hello/index.py
+++ hello/index.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 import json
-# import jwt
+import jwt
 
 # JWT_EXP_DELTA = 365
 # JWT_SECRET = 'BlaBla'
@@ -10,13 +10,13 @@ def my_func():
     print("I am index.py")
 # def lambda_handler(event, context):
 # 	return "Hello from Lambda!"
-    # payload = {
-    #     'username': 'sib',
-    #     'exp': datetime.utcnow() + timedelta(days=JWT_EXP_DELTA)
-    # }
+    payload = {
+        'username': 'sib',
+        'exp': datetime.utcnow() + timedelta(days=JWT_EXP_DELTA)
+    }
     
-    # #Encode the payload 
-    # encoded_token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGO)
-    # if encoded_token:
-    #     response_body = {'token': encoded_token}
-    #     return response_body
+    #Encode the payload 
+    encoded_token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGO)
+    if encoded_token:
+        response_body = {'token': encoded_token}
+        return response_body
