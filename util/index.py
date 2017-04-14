69d2766cd281c6b333f860bdf8917b1e1cfac506
sibtain10 <sibtain.abbas10@gmail.com>
2017-04-14 12:28:32 +0500

    copy a util file

--- util/index.py
+++ util/index.py
@@ -1,15 +1,13 @@
 from __future__ import print_function
-
+from datetime import datetime
 import json
 import jwt
 
-# JWT_EXP_DELTA = 365
-# JWT_SECRET = 'BlaBla'
-# JWT_ALGO = 'HS256'
+JWT_EXP_DELTA = 365
+JWT_SECRET = 'BlaBla'
+JWT_ALGO = 'HS256'
 def my_func():
     print("I am index.py")
-# def lambda_handler(event, context):
-# 	return "Hello from Lambda!"
     payload = {
         'username': 'sib',
         'exp': datetime.utcnow() + timedelta(days=JWT_EXP_DELTA)
